f709bab58d57bc48bb8040c4cbe1787aabf9bf25
ALPECS <99166566+ALPECS@users.noreply.github.com>
2023-04-26 18:24:58 -0700

    updates

--- AIPart.py
+++ AIPart.py
@@ -1,10 +1,23 @@
+import openai
+import re
+
 openai.api_key = 'pk-JsMTVtPJVasDqBDFMosXZqmbfTRPYxhWbvCWRqzhkTDGGpUe'
 openai.api_base = 'https://api.pawan.krd/v1'
 
-response = openai.Completion.create(
-    model="gpt-3.5-turbo",
-    temperature= "0",
-    prompt = "Rate this from a scale of 0 to 10, 0 being negative, 5 being nuteral 10 being positive: ' this product is okay '"
-)
 
-print(response.choices[0].text)
+def get_sentiment(text):
+    prompt_text = """Classify the sentiment of the following tweet as positive, negative, or neutral towards Microsoft. 
+    text: {}
+    sentiment: """.format(text)
+
+    sentiment = openai.Completion.create(
+        model="gpt-3.5-turbo",
+        prompt=prompt_text,
+        max_tokens=15,
+        temperature=0,
+    )
+
+    # remove special characters e.g n etc, from response
+    sentiment = re.sub('W+', '', sentiment['choices'][0]['text'])
+
+    return sentiment
--- testing.py
+++ testing.py
@@ -1,39 +1,58 @@
 import openai
 import os
 import pandas as pd
-
-
 import tweepy
+import openai
+import re
 
+
+# Method for getting data given a ticker
+def getData(ticker):
+    tweet_dataset = []
+    tweets = twitterAPI.search_tweets(q=ticker + ' -filter:retweets -filter:replies',
+                                      lang="en",
+                                      result_type="recent",
+                                      count=10
+                                      )
+    for tweet in tweets:
+        tweet_content = {'id': tweet.id,
+                         'date': tweet.created_at,
+                         'user_name': tweet.user.screen_name,
+                         'text': tweet.text,
+                         }
+
+        tweet_dataset.append(tweet_content)
+
+    tweet_dataframe = pd.DataFrame(tweet_dataset)
+    return tweet_dataframe
+
+
+def get_sentiment(text):
+    prompt_text = """Classify the sentiment of the following tweet as positive, negative, or neutral towards Microsoft. 
+    text: {}
+    sentiment: """.format(text)
+
+    sentiment = openai.Completion.create(
+        model="gpt-3.5-turbo",
+        prompt=prompt_text,
+        max_tokens=15,
+        temperature=0,
+    )
+
+    # remove special characters e.g n etc, from response
+    sentiment = re.sub('W+', '', sentiment['choices'][0]['text'])
+
+    return sentiment
+
+
+# Put in your tokens
 consumer_key = "BkEPb0jyEMmw878gj74vOrkFY"
 consumer_secret = "24dZiQhAZdTX4QFuZc9EZpfYJdnw83tzTARVq9KH0mITjQrcyE"
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAL4NnAEAAAAAp%2BpBIxt%2FjZja7nn5wEZtInIgCAs%3DFzdsKA1nkcN6OrdDlCGiuKHcpRDowA8lcNAmZT0FVwl5YefguE"
 
+# Connect to the Twitter API
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 twitterAPI = tweepy.API(auth)
 
-
-tweet_dataset = []
-tweets = twitterAPI.search_tweets(q='RDNT -filter:retweets -filter:replies',
-                                  lang = "en",
-                                  result_type="recent",
-                                  count = 10
-                                 )
-
-for tweet in tweets:
-    tweet_content = {'id': tweet.id,
-                     'date': tweet.created_at,
-                     'user_name': tweet.user.screen_name,
-                     'text': tweet.text,
-                     }
-
-    tweet_dataset.append(tweet_content)
-
-tweet_dataframe = pd.DataFrame(tweet_dataset)
-print(tweet_dataframe.shape)
-tweet_dataframe.head()
-
-
-
-
-
+# Get your pandas dataframe
+tweet_df = getData("RDNT")
